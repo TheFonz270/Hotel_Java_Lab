b02eb27a5ed8fd252057e35fa47fd8300b63e4cb
Martin Quinn <thefonz270@hotmail.com>
2022-01-13 15:56:10 +0000

    All Advanced Extensions Complete.

--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -9,11 +9,14 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     private Guest guest1;
-    private Bedroom bedroom;
+    private Guest guest2;
+    private Bedroom bedroom1;
+    private Bedroom bedroom2;
     private ConferenceRoom cRoom;
     private ArrayList<Bedroom> bedrooms;
     private ArrayList<ConferenceRoom> cRooms;
-    private ArrayList<Guest> uncheckedGuests;
+    private ArrayList<Guest> uncheckedGuests1;
+    private ArrayList<Guest> uncheckedGuests2;
     private Hotel cct;
     private Booking booking;
     private DiningRoom laChambre;
@@ -25,14 +28,19 @@ public class HotelTest {
     @Before
     public void before(){
         guest1 = new Guest("Jesse");
-        uncheckedGuests = new ArrayList<>();
-        uncheckedGuests.add(guest1);
-        bedroom = new Bedroom(1, RoomType.SINGLE, 7, 45.00 );
+        guest2 = new Guest("James");
+        uncheckedGuests1 = new ArrayList<>();
+        uncheckedGuests1.add(guest1);
+        uncheckedGuests2 = new ArrayList<>();
+        uncheckedGuests2.add(guest2);
+        bedroom1 = new Bedroom(1, RoomType.SINGLE, 7, 45.00 );
+        bedroom2 = new Bedroom(1, RoomType.SINGLE, 8, 40.00 );
         cRoom = new ConferenceRoom(50, "Orkney");
         bedrooms = new ArrayList<>();
         cRooms = new ArrayList<>();
         cRooms.add(cRoom);
-        bedrooms.add(bedroom);
+        bedrooms.add(bedroom1);
+        bedrooms.add(bedroom2);
         laChambre = new DiningRoom("La Chambre", 20);
         laSalleAManger = new DiningRoom("La Salle a Manger", 50);
         laCuisine = new DiningRoom("La Cuisine", 15);
@@ -41,7 +49,7 @@ public class HotelTest {
         diningRooms.put("La Salle a Manger", laSalleAManger);
         diningRooms.put("La Cuisine", laCuisine);
         cct = new Hotel(bedrooms, cRooms, diningRooms);
-        booking = new Booking(bedroom, 5);
+        booking = new Booking(bedroom1, 5);
     }
 
     @Test
@@ -51,16 +59,16 @@ public class HotelTest {
 
     @Test
     public void canCheckInSingle(){
-    cct.checkIn(uncheckedGuests, RoomType.SINGLE );
-    assertEquals(bedroom.getGuest(0).getName(), "Jesse");
+    cct.checkIn(uncheckedGuests1, RoomType.SINGLE );
+    assertEquals(bedroom1.getGuest(0).getName(), "Jesse");
     }
 
     @Test
     public void canCheckOutGuests() {
-        cct.checkIn(uncheckedGuests, RoomType.SINGLE);
-        assertEquals(1, bedroom.getGuestNumber());
+        cct.checkIn(uncheckedGuests1, RoomType.SINGLE);
+        assertEquals(1, bedroom1.getGuestNumber());
         cct.checkOut(7);
-        assertEquals(0, bedroom.getGuestNumber());
+        assertEquals(0, bedroom1.getGuestNumber());
     }
 
     @Test
@@ -79,6 +87,26 @@ public class HotelTest {
     @Test
     public void HotelHasDiningRooms(){
         assertEquals(cct.getDiningRoom("La Salle a Manger").getName(), "La Salle a Manger");
+    }
+
+    @Test
+    public void canGetVacantRooms(){
+        assertEquals(cct.getVacantRooms().size(), 2);
+        cct.checkIn(uncheckedGuests1, RoomType.SINGLE);
+        assertEquals(cct.getVacantRooms().size(), 1);
+
+    }
+
+    @Test
+    public void CanCheckInOnlyVacant() {
+        assertEquals(0, bedroom1.getGuestNumber());
+        assertEquals(0, bedroom2.getGuestNumber());
+        cct.checkIn(uncheckedGuests1, RoomType.SINGLE);
+        assertEquals(1, bedroom1.getGuestNumber());
+        assertEquals(0, bedroom2.getGuestNumber());
+        cct.checkIn(uncheckedGuests2, RoomType.SINGLE);
+        assertEquals(1, bedroom1.getGuestNumber());
+        assertEquals(1, bedroom2.getGuestNumber());
     }
 }
 
